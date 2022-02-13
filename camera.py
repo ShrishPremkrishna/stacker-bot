74ad612ad5005b3f9d63c945ef4ae2d69b292b33
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-02-13 02:19:00 -0800

    Update camera.py

--- camera.py
+++ camera.py
@@ -24,21 +24,12 @@ class VideoCamera(object):
             self.runner.stop()
 
     def get_frame(self):
-        res, img = self.runner.classifier(0)
+        for res, img in self.runner.classifier(0):
+            if "bounding_boxes" in res["result"].keys():
+                print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
+                for bb in res["result"]["bounding_boxes"]:
+                    print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
+                    img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
 
-        # if "classification" in res["result"].keys():
-        #     print('Result (%d ms.) ' % (res['timing']['dsp'] + res['timing']['classification']), end='')
-        #     for label in self.labels:
-        #         score = res['result']['classification'][label]
-        #         print('%s: %.2f\t' % (label, score), end='')
-        #     print('', flush=True)
-
-        if "bounding_boxes" in res["result"].keys():
-            print('Found %d bounding boxes (%d ms.)' % (len(res["result"]["bounding_boxes"]), res['timing']['dsp'] + res['timing']['classification']))
-            for bb in res["result"]["bounding_boxes"]:
-                print('\t%s (%.2f): x=%d y=%d w=%d h=%d' % (bb['label'], bb['value'], bb['x'], bb['y'], bb['width'], bb['height']))
-                img = cv2.rectangle(img, (bb['x'], bb['y']), (bb['x'] + bb['width'], bb['y'] + bb['height']), (255, 0, 0), 1)
-
-        ret, jpeg = cv2.imencode('.jpg', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-
-        return jpeg.tobytes()
+            ret, jpeg = cv2.imencode('.jpg', cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            return jpeg.tobytes()
