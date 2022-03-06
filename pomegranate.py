73f7243735d3aa894cad8419e523f09a6fa8ea95
ShrishPremkrishna <shrish.premkrishna@gmail.com>
2022-03-05 22:39:39 -0800

    Update pomegranate.py

--- pomegranate.py
+++ pomegranate.py
@@ -2,14 +2,35 @@ from pyPS4Controller.controller import Controller
 from roboclaw import Roboclaw
 from PCA9685 import PCA9685
 from time import sleep
+import time
+import cv2
+import calendar 
 
 speed = 20
 rest = 0
 
 class MyController(Controller):
-
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
+
+    def stopChassis(self):
+        sleep(0.5)
+        roboclaw.ForwardM1(0x80,rest)
+        roboclaw.ForwardM2(0x80,rest)
+        roboclaw.ForwardM1(0x81,rest)
+        roboclaw.ForwardM2(0x81,rest)
+
+    def savePic(self):
+        cam = cv2.VideoCapture(0)
+        gmt = time.gmtime()
+        ts = calendar.timegm(gmt)
+        result, frame = cam.read()
+        if not result:
+            print("failed to grab frame")
+        img_name = "image_{}.jpg".format(ts)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        cam.release()        
 
     #Up
     def on_triangle_press(self):
@@ -18,11 +39,7 @@ class MyController(Controller):
         roboclaw.BackwardM2(0x80,speed)
         roboclaw.BackwardM1(0x81,speed)
         roboclaw.BackwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
 
     #Down
     def on_x_press(self):
@@ -31,11 +48,8 @@ class MyController(Controller):
         roboclaw.ForwardM2(0x80,speed)
         roboclaw.ForwardM1(0x81,speed)
         roboclaw.ForwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
+
 
     #Right
     def on_circle_press(self):
@@ -44,11 +58,8 @@ class MyController(Controller):
         roboclaw.BackwardM2(0x80,speed)
         roboclaw.BackwardM1(0x81,speed)
         roboclaw.ForwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
+
 
     #Left
     def on_square_press(self):
@@ -57,11 +68,8 @@ class MyController(Controller):
         roboclaw.ForwardM2(0x80,speed)
         roboclaw.ForwardM1(0x81,speed)
         roboclaw.BackwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
+
 
     #RotateRight
     def on_right_arrow_press(self):
@@ -70,11 +78,8 @@ class MyController(Controller):
         roboclaw.ForwardM2(0x80,speed)
         roboclaw.BackwardM1(0x81,speed)
         roboclaw.ForwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
+
 
     #RotateLeft
     def on_left_arrow_press(self):
@@ -83,11 +88,8 @@ class MyController(Controller):
         roboclaw.BackwardM2(0x80,speed)
         roboclaw.ForwardM1(0x81,speed)
         roboclaw.BackwardM2(0x81,speed)
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,rest)
-        roboclaw.ForwardM2(0x80,rest)
-        roboclaw.ForwardM1(0x81,rest)
-        roboclaw.ForwardM2(0x81,rest)
+        self.stopChassis()
+
 
 
 if __name__ == "__main__":
