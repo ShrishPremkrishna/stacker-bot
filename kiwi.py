fe05dc1442106170a0ae6669cc0f77ccf6aa97f7
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-03-20 20:08:25 -0700

    Update kiwi.py

--- kiwi.py
+++ kiwi.py
@@ -13,10 +13,10 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-        self.cam_pulse = 2700
+        self.cam_pulse = 2800
         self.cam_channel = 4
-        self.cam_max = 2700
-        self.cam_min = 2700
+        self.cam_max = 2800
+        self.cam_min = 2800
         self.pwm = PCA9685(0x40, debug=False)
         self.pwm.setPWMFreq(50)
         print("camera pulse being initiated at " + str(self.cam_pulse))
