d009669746b48b188f359d7b5cc0ffbfa94d52f7
Shrish Premkrishna <shrish.premkrishna@gmail.com>
2022-03-27 03:30:38 -0700

    Update kiwi.py

--- kiwi.py
+++ kiwi.py
@@ -13,10 +13,10 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-        self.cam_pulse = 2800
+        self.cam_pulse = 2400
         self.cam_channel = 4
-        self.cam_max = 2800
-        self.cam_min = 2800
+        self.cam_max = 2400
+        self.cam_min = 2400
         self.pwm = PCA9685(0x40, debug=False)
         self.pwm.setPWMFreq(50)
         print("camera pulse being initiated at " + str(self.cam_pulse))
@@ -25,27 +25,15 @@ class MyController(Controller):
         self.motorSpeed = 20
         self.motorRest = 0
 
-        
-
-    # def __del__(self):
-        # self.cam.release()
-
-    def stopChassis(self):
-        sleep(0.5)
-        roboclaw.ForwardM1(0x80,self.motorRest)
-        roboclaw.ForwardM2(0x80,self.motorRest)
-        roboclaw.ForwardM1(0x81,self.motorRest)
-        roboclaw.ForwardM2(0x81,self.motorRest)
-
     def resetCamPosition (self):
         self.cam_pulse = self.cam_max
         self.pwm.setServoPulse(self.cam_channel, self.cam_pulse)
         print("Cam pulse at - " + str(self.cam_pulse))
 
-    def prepCam(self):
+    def prepCam(self, label):
         if (self.cam_pulse != self.cam_max):
             self.resetCamPosition()
-        self.savePic()
+        self.savePic(label)
         while (self.cam_pulse > self.cam_min) :
             # Lowers camera to a 100 pulse
             for i in range(self.cam_pulse, self.cam_pulse - 100, -10):  
@@ -57,7 +45,7 @@ class MyController(Controller):
         self.resetCamPosition()
 
 
-    def savePic(self):
+    def savePic(self, label):
         cam = cv2.VideoCapture(0)
         ts = str(time.time()).replace(".", "_")
         # print('predelay')
@@ -65,12 +53,15 @@ class MyController(Controller):
         result, frame = cam.read()
         if not result:
             print("failed to grab frame")
-        img_name = "images/image_{}.jpg".format(ts)
+        img_name = "images/{}-position_{}.jpg".format(label, ts)
         pts1 = np.float32([[0,0],[640,0],[0,480],[640,480]])
         pts2 = np.float32([[0,0],[320,0],[0,320],[320,320]])
         M = cv2.getPerspectiveTransform(pts1,pts2)
         frame = cv2.warpPerspective(frame,M,(320,320))
         cv2.imwrite(img_name, frame)
+        cv2.show(img_name, frame)
+        if cv2.waitKey(300) == 27: 
+            print("end wait key")
         print("{} written!".format(img_name), frame.shape)
         # print('postdelay')
         # time.sleep(0.2)
@@ -78,67 +69,38 @@ class MyController(Controller):
         
                 
 
-    #Up
+    #Rotate
     def on_triangle_press(self):
         print("Up")
-        roboclaw.BackwardM1(0x80,self.motorSpeed)
-        roboclaw.BackwardM2(0x80,self.motorSpeed)
-        roboclaw.BackwardM1(0x81,self.motorSpeed)
-        roboclaw.BackwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
-
-    #Down
-    def on_x_press(self):
-        print("Down")
-        roboclaw.ForwardM1(0x80,self.motorSpeed)
-        roboclaw.ForwardM2(0x80,self.motorSpeed)
-        roboclaw.ForwardM1(0x81,self.motorSpeed)
-        roboclaw.ForwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
-
-
-    #Right
-    def on_circle_press(self):
-        print("Right")
         roboclaw.ForwardM1(0x80,self.motorSpeed)
         roboclaw.BackwardM2(0x80,self.motorSpeed)
         roboclaw.BackwardM1(0x81,self.motorSpeed)
         roboclaw.ForwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
-
-
-    #Left
-    def on_square_press(self):
-        print("Left")
-        roboclaw.BackwardM1(0x80,self.motorSpeed)
-        roboclaw.ForwardM2(0x80,self.motorSpeed)
-        roboclaw.ForwardM1(0x81,self.motorSpeed)
-        roboclaw.BackwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
-
-
-    #RotateRight
-    def on_right_arrow_press(self):
-        print("RotateRight")
-        roboclaw.BackwardM1(0x80,self.motorSpeed)
-        roboclaw.ForwardM2(0x80,self.motorSpeed)
-        roboclaw.BackwardM1(0x81,self.motorSpeed)
-        roboclaw.ForwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
-
-
-    #RotateLeft
-    def on_left_arrow_press(self):
-        print("RotateLeft")
+        time.sleep(0.35)
+        roboclaw.ForwardM1(0x80,0)
+        roboclaw.ForwardM2(0x80,0)
+        roboclaw.ForwardM1(0x81,0)
+        roboclaw.ForwardM2(0x81,0)
+        time.sleep(0.15)
         roboclaw.ForwardM1(0x80,self.motorSpeed)
         roboclaw.BackwardM2(0x80,self.motorSpeed)
         roboclaw.ForwardM1(0x81,self.motorSpeed)
         roboclaw.BackwardM2(0x81,self.motorSpeed)
-        self.stopChassis()
+        time.sleep(0.35)
+        roboclaw.ForwardM1(0x80,0)
+        roboclaw.ForwardM2(0x80,0)
+        roboclaw.ForwardM1(0x81,0)
+        roboclaw.ForwardM2(0x81,0)
+        time.sleep(0.15)
+        # self.stopChassis()
 
     def on_up_arrow_press(self):
         print("Still Picture")
-        self.prepCam()
+        self.prepCam("in")
+    
+    def on_down_arrow_press(self):
+        print("Still Picture")
+        self.prepCam("out-of")
 
 
 
@@ -152,12 +114,6 @@ if __name__ == "__main__":
         print('Unable to open port')
     print('Printing connection result - ' + str(result))
     print('Connection - ' + str(roboclaw._port.is_open))
-    
-    # pwm = PCA9685(0x40, debug=False)
-    # pwm.setPWMFreq(50)
-    # pwm.setPWM(0, 0, 4096)
-    # pwm.setPWM(2, 0, 4096)
-    # pwm.setPWM(4, 0, 4096)
 
     controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
     controller.listen(timeout=6)
